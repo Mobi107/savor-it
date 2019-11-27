f4b065641d708e3c010fe33318274e5975b6afdf
amraharif <arif.amrah@berkeley.edu>
2019-11-26 16:17:22 -0800

    working on fixing null error in search

---
+++ app/src/main/java/com/example/checknavview/ui/request/RequestViewModel.java
@@ -0,0 +1,19 @@
+package com.example.checknavview.ui.request;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class RequestViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public RequestViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is request fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
