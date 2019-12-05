cf81d316215201191161680296bce13738912cce
Duy Nguyen <duy@berkeley.edu>
2019-12-05 00:07:41 -0800

    change fragment

--- app/src/main/java/com/example/savor_it/ui/home/UploadFragment.java
+++ app/src/main/java/com/example/savor_it/ui/home/UploadFragment.java
@@ -1,15 +1,28 @@
 package com.example.savor_it.ui.home;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.annotation.AnyRes;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.savor_it.R;
@@ -18,6 +31,7 @@ import java.util.ArrayList;
 
 public class UploadFragment extends Fragment {
 
+    private static final int REQUEST_CODE_PHOTO = 100;
     EditText et;
     EditText step;
     Button add_btn;
@@ -28,6 +42,8 @@ public class UploadFragment extends Fragment {
     ArrayList<String> ingredients;
     ArrayAdapter<String> stepsAdapter;
     ArrayList<String> steps;
+    ImageView imageView;
+    private Uri imageUri;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -40,6 +56,7 @@ public class UploadFragment extends Fragment {
         second_add_btn = (Button) root.findViewById(R.id.add_step_btn);
         listView = (ListView) root.findViewById(R.id.listView);
         secondListView = (ListView) root.findViewById(R.id.add_step_list_view);
+        imageView = root.findViewById(R.id.imageView2);
 
         ingredients = new ArrayList<>();
         ingredientsAdapter = new ArrayAdapter<>(this.getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, ingredients);
@@ -48,6 +65,7 @@ public class UploadFragment extends Fragment {
         steps = new ArrayList<>();
         stepsAdapter = new ArrayAdapter<>(this.getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, steps);
         secondListView.setAdapter(stepsAdapter);
+
         onAddButtonClick();
 
         return root;
@@ -71,5 +89,69 @@ public class UploadFragment extends Fragment {
                 stepsAdapter.notifyDataSetChanged();
             }
         });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectEventImage(v);
+            }
+        });
     }
+
+
+    private void selectEventImage(View v) {
+        // List of all the permissions to request from user
+        ArrayList<String> permList = new ArrayList<>();
+
+        // Check to see if permissions to camera has been granted
+        int cameraPermission = ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA);
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED)
+            permList.add(android.Manifest.permission.CAMERA);
+
+        // Check if permissions to camera is granted
+        int readExternalStoragePermission = ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (readExternalStoragePermission != PackageManager.PERMISSION_GRANTED)
+            permList.add(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        // Check if permissions to writing external storage is granted
+        int writeExternalStoragePermission = ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (writeExternalStoragePermission != PackageManager.PERMISSION_GRANTED)
+            permList.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        // If the list has items (size > 0), request permissions from the user:
+        if (permList.size() > 0)
+        {
+            // Convert ArrayList into an Array of Strings
+            String[] perms = new String[permList.size()];
+            // Request permission from user
+            ActivityCompat.requestPermissions(getActivity(), permList.toArray(perms), REQUEST_CODE_PHOTO);
+        }
+
+        // If we have all 3 permissions, open ImageGallery
+        if (cameraPermission == PackageManager.PERMISSION_GRANTED
+                && readExternalStoragePermission == PackageManager.PERMISSION_GRANTED
+                && writeExternalStoragePermission == PackageManager.PERMISSION_GRANTED)
+        {
+            // Start an intent to launch gallery and take pics
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, REQUEST_CODE_PHOTO);
+        } else {
+            Toast.makeText(getContext(),
+                    "OCC Community Service Hours requires camera and external storage permission",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data != null && requestCode == REQUEST_CODE_PHOTO && resultCode == Activity.RESULT_OK)
+        {
+            // Set the imageURI to the data
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
+
+
 }
