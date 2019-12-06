b09184dd01a9968f84d57a6cfcff194e0e9ebf78
Duy Nguyen <duy@berkeley.edu>
2019-12-05 21:47:50 -0800

    request

--- app/src/main/java/com/example/savor_it/ui/request/RequestFragment.java
+++ app/src/main/java/com/example/savor_it/ui/request/RequestFragment.java
@@ -4,32 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.savor_it.R;
+import com.example.savor_it.model.Request;
+import com.example.savor_it.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RequestFragment extends Fragment {
 
-    private RequestViewModel requestViewModel;
+    private Spinner spinnerUserName;
+    private EditText editText;
+    private Button requestButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        requestViewModel =
-                ViewModelProviders.of(this).get(RequestViewModel.class);
         View root = inflater.inflate(R.layout.fragment_request, container, false);
-        final TextView textView = root.findViewById(R.id.textView);
-        requestViewModel.getText().observe(this, new Observer<String>() {
+        spinnerUserName = root.findViewById(R.id.userNameSpinner);
+        editText = root.findViewById(R.id.recipeNameRequesteditText);
+        requestButton = root.findViewById(R.id.requestButton);
+        final List<String> username = new ArrayList<>();
+        username.add("Mom");
+        username.add("Aunt");
+        spinnerUserName.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, username));
+        requestButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                String recipeName = editText.getText().toString();
+
+                if (recipeName.length() != 0 && spinnerUserName.getSelectedItemPosition() != 0) {
+                    String recipentId = (String) spinnerUserName.getSelectedItem();
+                    Request request = new Request("mom", recipentId, recipeName);
+                }
             }
         });
         return root;
     }
+
+
+
 }
--- app/src/main/java/com/example/savor_it/util/DBHelper.java
+++ app/src/main/java/com/example/savor_it/util/DBHelper.java
@@ -10,21 +10,39 @@ class DBHelper extends SQLiteOpenHelper {
     private Context mContext;
 
     //TASK: DEFINE THE DATABASE VERSION AND NAME  (DATABASE CONTAINS MULTIPLE TABLES)
-    static final String DATABASE_NAME = "OCC COMMUNITY SERVICE"; //remember change to public later
+    static final String DATABASE_NAME = "SAVOR IT"; //remember change to public later
     private static final int DATABASE_VERSION = 1;
 
     //TASK: DEFINE THE FIELDS (COLUMN NAMES) FOR THE USERS TABLE
-    private static final String USERS_TABLE = "Courses";
+    private static final String USERS_TABLE = "Users";
     private static final String USERS_KEY_FIELD_ID = "_id";
-    private static final String FIELD_FIRST_NAME = "first_name";
-    private static final String FIELD_LAST_NAME = "last_name";
     private static final String FIELD_USERNAME = "username";
     private static final String FIELD_EMAIL = "email";
     private static final String FIELD_PHONE_NUMBER = "phone_number";
     private static final String FIELD_PASSWORD = "password";
-    private static final String FIELD_HOURS = "hours";
-    private static final String FIELD_ROLE = "role";
     private static final String FIELD_IMAGE_NAME = "image_name";
+
+
+    //TASK: DEFINE THE FIELDS (COLUMN NAMES) FOR THE LOGIN TABLE
+    private static final String LOGIN_TABLE = "Login";
+    private static final String LOGIN_KEY_FIELD_ID = "_id";
+    private static final String FIELD_LOGIN_USER_ID = "user_id";
+    private static final String FIELD_LOGIN_USER_ROLE = "role";
+
+
+
+    // FIELD NAMES FOR THE EVENTS TABLE
+    private static final String RECIPE_TABLE = "RECIPES";
+    private static final String EVENTS_KEY_FIELD_ID = "id";
+    private static final String FIELD_NAME = "name";
+    private static final String FIELD_OWNER_ID = "owner_id";
+    private static final String FIELD_INGREDIENTS = "start_date";
+    private static final String FIELD_END_DATE = "end_date";
+    private static final String FIELD_DESCRIPTION = "description";
+    private static final String FIELD_LOCATION = "location";
+    private static final String FIELD_EVENT_IMAGE_NAME = "image_name";
+
+
 
 
     public DBHelper(Context context) {
@@ -34,6 +52,14 @@ class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String createQuery = "CREATE TABLE " + USERS_TABLE + "("
+                + USERS_KEY_FIELD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + FIELD_USERNAME + " TEXT UNIQUE, "
+                + FIELD_EMAIL + " TEXT UNIQUE, "
+                + FIELD_PHONE_NUMBER + " TEXT, "
+                + FIELD_PASSWORD + " TEXT, "
+                + FIELD_IMAGE_NAME + " TEXT" + ")";
+        db.execSQL(createQuery);
 
     }
 
